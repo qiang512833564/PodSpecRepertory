fe0b2e680aa66ea335ae460e94d5cda30f3ac510
HW <hw@zhangweideMacBook-Pro.local>
2017-05-23 10:50:21 +0800

    Initial Commit of Library
    Signed-off-by: HW <hw@zhangweideMacBook-Pro.local>

---
+++ Example/Pods/Target Support Files/PodSpecRepertory/PodSpecRepertory-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PodSpecRepertory : NSObject
+@end
+@implementation PodsDummy_PodSpecRepertory
+@end
---
+++ Example/Pods/Target Support Files/PodSpecRepertory/PodSpecRepertory-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double PodSpecRepertoryVersionNumber;
+FOUNDATION_EXPORT const unsigned char PodSpecRepertoryVersionString[];
+
